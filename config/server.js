2ffd8e21d6a29e6304e0b6c984ced39465b9001d
sketchypages <116029342+sketchypages@users.noreply.github.com>
2023-03-28 10:16:13 +0100

    modified server.js

--- config/server.js
+++ config/server.js
@@ -1,5 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
+  // host: env("HOST", "0.0.0.0"),
+  host: env("https://nblblog.herokuapp.com/"),
+  proxy: true,
   port: env.int("PORT", 1337),
   app: {
     keys: env.array("APP_KEYS", ["banana", "apple"]),
